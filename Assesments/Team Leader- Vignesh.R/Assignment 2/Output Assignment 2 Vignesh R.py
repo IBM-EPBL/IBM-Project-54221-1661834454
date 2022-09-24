49771635aea95dd84f9eac35da27846f74ef2b29
Vignesh24Rv <113709390+Vignesh24Rv@users.noreply.github.com>
2022-09-24 11:21:14 +0530

    Add files via upload

---
+++ Assesments/Team Leader- Vignesh.R/Assignment 2/Output Assignment 2 Vignesh R.py
@@ -0,0 +1,25 @@
+Python 3.7.0 (v3.7.0:1bf9cc5093, Jun 27 2018, 04:59:51) [MSC v.1914 64 bit (AMD64)] on win32
+Type "copyright", "credits" or "license()" for more information.
+>>> 
+ RESTART: C:/Users/Nalla pulla/AppData/Local/Programs/Python/Python37/python code assignment 2.py 
+Low temperature and humidity of: 33 2 % alarm is off
+>>> 
+ RESTART: C:/Users/Nalla pulla/AppData/Local/Programs/Python/Python37/python code assignment 2.py 
+high temperature and humidity of: 53 69 % alarm is on
+Low temperature and humidity of: 27 38 % alarm is off
+>>> 
+ RESTART: C:/Users/Nalla pulla/AppData/Local/Programs/Python/Python37/python code assignment 2.py 
+Low temperature and humidity of: 17 32 % alarm is off
+>>> 
+ RESTART: C:/Users/Nalla pulla/AppData/Local/Programs/Python/Python37/python code assignment 2.py 
+high temperature and humidity of: 90 72 % alarm is on
+Low temperature and humidity of: 25 20 % alarm is off
+>>> 
+ RESTART: C:/Users/Nalla pulla/AppData/Local/Programs/Python/Python37/python code assignment 2.py 
+high temperature and humidity of: 86 45 % alarm is on
+high temperature and humidity of: 91 86 % alarm is on
+high temperature and humidity of: 46 43 % alarm is on
+high temperature and humidity of: 61 93 % alarm is on
+high temperature and humidity of: 57 68 % alarm is on
+Low temperature and humidity of: 11 27 % alarm is off
+>>> 
